c1c6a46c7e744adf58fc5a5a2bd7ae3450ea74b7
Katrin Marschner <katrin.marschner@gmail.com>
2019-07-20 11:43:30 +0200

    starter code for observer

---
+++ observer/employee.rb
@@ -0,0 +1,16 @@
+class Employee
+  attr_reader :name, :salary
+  attr_accessor :title
+
+  def initialize(name, title, salary, payroll)
+    @name = name
+    @title = title
+    @salary = salary
+    @payroll = payroll
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+    @payroll.update(self)
+  end
+end
