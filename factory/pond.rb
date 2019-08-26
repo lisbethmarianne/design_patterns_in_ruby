d27f6ff1601ddf24c44034bf11872bb4b6c26c20
Katrin Marschner <katrin.marschner@gmail.com>
2019-08-26 15:31:04 +0200

    move factory method to creator

--- factory/pond.rb
+++ factory/pond.rb
@@ -1,5 +1,8 @@
 class Pond
-  def initialize(number_animals, number_plants)
+  def initialize(number_animals, animal_class, number_plants, plant_class)
+    @animal_class = animal_class
+    @plant_class = plant_class
+
     @animals = []
     number_animals.times do |i|
       animal = new_organism(:animal, "Animal#{i}")
@@ -18,5 +21,15 @@ class Pond
     @animals.each { |animal| animal.speak }
     @animals.each { |animal| animal.eat }
     @animals.each { |animal| animal.sleep }
+  end
+
+  def new_organism(type, name)
+    if type == :animal
+      @animal_class.new(name)
+    elsif type == :plant
+      @plant_class.new(name)
+    else
+      raise "Unknown organism type: #{type}"
+    end
   end
 end
