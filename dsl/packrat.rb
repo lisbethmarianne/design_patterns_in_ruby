a8843ac4286be4177837c2061992195df9bbdfd2
Katrin Marschner <katrin.marschner@gmail.com>
2019-08-29 09:19:09 +0200

    backup files with packrat

--- dsl/packrat.rb
+++ dsl/packrat.rb
@@ -1,15 +1,18 @@
 require_relative 'file_finder'
+require_relative 'backup'
+require_relative 'data_source'
 
 def backup(dir, find_expression=All.new)
-  puts "Backup called, source dir=#{dir} find expr=#{find_expression}"
+  Backup.instance.data_sources << DataSource.new(dir, find_expression)
 end
 
 def to(backup_directory)
-  puts "To called, backup_dir=#{backup_directory}"
+  Backup.instance.backup_directory = backup_directory
 end
 
 def interval(minutes)
-  puts "Interval called, interval=#{minutes} minutes"
+  Backup.instance.interval = minutes
 end
 
 eval(File.read('dsl/backup.pr'))
+Backup.instance.run
